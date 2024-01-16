daa6858a02930e5e914200ad74f3b1843a129695
Md. Abdus Salam <salaam.mbstu@gmail.com>
2024-01-16 11:19:23 +0600

    Added Java based configurations

---
+++ task-tracker/src/main/java/com/example/tasktracker/configurations/WebAppConfig.java
@@ -0,0 +1,25 @@
+package com.example.tasktracker.configurations;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebAppConfig implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+
+        context.register(WebMvcConfig.class);
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(context);
+        ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcherServlet", dispatcherServlet);
+
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+
+}
